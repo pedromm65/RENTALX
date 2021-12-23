92ae2ccf2e47162282edd1d786fc249bd77ce5a8
pedromm65 <pedromm65@hotmail.com.br>
2021-12-23 18:36:27 -0300

    Criando a rota specification

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,25 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+    name;
+    description: string;
+}
+
 class CreateSpecificationService {
-    execute() {
-        return null;
+    constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+    execute({ name, description }: IRequest): void {
+        const specificationsAlreadyExists =
+            this.specificationsRepository.findByName(name);
+
+        if (specificationsAlreadyExists) {
+            throw new Error("Specification already exists!");
+        }
+
+        this.specificationsRepository.create({
+            name,
+            description,
+        });
     }
 }
 
