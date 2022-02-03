1fcc1a3660632a1d407300f0d5efa26afb5a6e9d
pedromm65 <pedromm65@hotmail.com.br>
2022-02-03 13:13:05 -0300

    Corrigindo o erro que fazia com que o Docker não desse restart automaticamente

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -48,10 +48,8 @@ class ImportCategoryUseCase {
 
     async execute(file: Express.Multer.File): Promise<void> {
         const categories = await this.loadCategories(file);
-
         categories.map(async (category) => {
             const { name, description } = category;
-
             const existCategory = await this.categoriesRepository.findByName(
                 name
             );
