f45db9d0c8bf386f1dd8b986f3780b0841f333e4
pedromm65 <pedromm65@hotmail.com.br>
2022-02-06 21:57:06 -0300

    Adicionando o avatar para o usuario

--- src/database/migrations/1644036229842-CreateUsers.ts
+++ src/database/migrations/1644036229842-CreateUsers.ts
@@ -4,7 +4,7 @@ export class CreateUsers1644036229842 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: "Users",
+                name: "users",
                 columns: [
                     {
                         name: "id",
@@ -47,6 +47,6 @@ export class CreateUsers1644036229842 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("Users");
+        await queryRunner.dropTable("users");
     }
 }
