092fd83e8b8cc7d27878a528a82fff1379a94882
pedromm65 <pedromm65@hotmail.com.br>
2022-03-30 11:13:45 -0300

    AAAA

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "3bbccd2f65dc83282dfa7643a7de9a30",
+    expires_in_token: "15m",
+    secrete_refresh_token: "c5bf84e00c543383e4194453c72fe057",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30,
+};
---
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -0,0 +1,13 @@
+import { UserTokens } from "@modules/accounts/infra/typeorm/entities/UserTokens";
+
+import { ICreateUserTokenDTO } from "../dtos/ICreateUserTokenDTO";
+
+interface IUsersTokensRepository {
+    create({
+        expires_date,
+        refresh_token,
+        user_id,
+    }: ICreateUserTokenDTO): Promise<UserTokens>;
+}
+
+export { IUsersTokensRepository };
