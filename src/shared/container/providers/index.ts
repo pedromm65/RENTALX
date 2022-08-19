dc1736d9b3e84b56121462fceb6c00cf75d94e10
Pedro Lima <pedromm65@hotmail.com.br>
2022-08-19 19:29:33 -0300

    Fixing users routes

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,8 +2,15 @@ import { container } from "tsyringe";
 
 import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
+import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 container.registerSingleton<IDateProvider>(
     "DayjsDateProvider",
     DayjsDateProvider
 );
+
+container.registerSingleton<IStorageProvider>(
+    "StorageProvider",
+    LocalStorageProvider
+)
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -17,7 +17,7 @@ const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpeficicationController();
 const uploadCarImagesController = new UploadCarImageController();
 
-const upload = multer(uploadConfig.upload("./tmp/car"));
+const upload = multer(uploadConfig);
 
 carsRoutes.post(
     "/",
